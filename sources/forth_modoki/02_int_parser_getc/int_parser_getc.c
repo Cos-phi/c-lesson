67e670815dfa7325ed622df93b698087e701b2bc
Cosphi <cosphi@kurajo.ivory.ne.jp>
2022-09-05 01:16:10 +0900

    とりあえず出題だけは満たしている状態？

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -10,33 +10,26 @@ enum jikutype {
     SPACE  = 1
 };
 
-char parse_one(int *out_val, int *out_type) {
-    int c;
-    int i = 0; 
-    while((c = cl_getc()) != EOF) {
-//        printf("c is %d\n",c);
+char parse_one(char c, int *out_val, enum jikutype *out_type) {
+    do{
         enum jikutype out_type_prev = *out_type;
         if (c == 32){
             *out_type = SPACE;
-//            printf("space\n");
         }else{
             *out_type = NUMBER;
             *out_val *= 10;
             *out_val += c - 48;
- //           printf("number\n");
         }
-        if (*out_type == SPACE && out_type_prev != SPACE) break;
- //       printf("type %d\n",*out_type);
-        printf("outval is %d\n",*out_val);
-    }
-    out_val[i];
-    return *out_val;
+        if (*out_type != out_type_prev) break;
+    }while((c = cl_getc()) != EOF);
+    return c;
 }
 
 
 
 int main() {
     int answer1 = 0;
+    int space = 0;
     int answer2 = 0;
 
     // write something here.
@@ -47,20 +40,17 @@ int main() {
     //    printf("%c\n",c );
     //}
 
-    printf("aaa\n");
-    int out_val = 0;
-    int out_type = 0;
-    int c = parse_one(&out_val,&out_type);
-    answer1 = out_val;
-    printf("returned c is %d\n",c);
-    printf("answer1 is %d\n",answer1);
+    char c = cl_getc();
+    enum jikutype out_type;
+    if (c == 32){
+        out_type = SPACE;
+    }else{
+        out_type = NUMBER;
+    }
 
-    out_val = 0;
-    out_type = 0;
-    c = parse_one(&out_val,&out_type);
-    printf("returned c is %d\n",c);
-    answer2 = out_val;
-    printf("answer2 is %d\n",answer2);
+    c = parse_one(c,&answer1,&out_type);
+    c = parse_one(c,&space,&out_type);
+    c = parse_one(c,&answer2,&out_type);
 
     // verity result.
     assert(answer1 == 123);
