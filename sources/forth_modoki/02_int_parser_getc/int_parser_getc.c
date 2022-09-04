4fb5a494b1c5b11e7f6cc6e895b5afd61e74fc89
Cosphi <cosphi@kurajo.ivory.ne.jp>
2022-09-05 00:08:05 +0900

    関数の動作確認

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -5,6 +5,33 @@
 cc cl_getc.c int_parser_getc.c
 */
 
+enum jikutype {
+    NUMBER = 0,
+    SPACE  = 1
+};
+
+char parse_one(int *out_val, int *out_type) {
+    int c;
+    int i = 0; 
+    while((c = cl_getc()) != EOF) {
+        printf("c is %d\n",c);
+        enum jikutype out_type_prev = *out_type;
+        if (c == 32){
+            *out_type = SPACE;
+            printf("space\n");
+        }else{
+            *out_type = NUMBER;
+            printf("number\n");
+        }
+        if (*out_type != out_type_prev) //break;
+        printf("%c:type %d\n",c,*out_type);
+    }
+    out_val[i];
+    return *out_val;
+}
+
+
+
 int main() {
     int answer1 = 0;
     int answer2 = 0;
@@ -12,11 +39,16 @@ int main() {
     // write something here.
 
     // sample for cl_getc() usage.
-    int c;
+    //int c;
+    //while((c = cl_getc()) != EOF) {
+    //    printf("%c\n",c );
+    //}
 
-    while((c = cl_getc()) != EOF) {
-        printf("%c\n",c );
-    }
+    printf("aaa\n");
+    int out_val;
+    int out_type = 1;
+    parse_one(&out_val,&out_type);
+    printf("outtype:%d\n",out_type);
 
     // verity result.
     assert(answer1 == 123);
