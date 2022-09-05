99b74a6b8a999896ac0250b5e1a64932d986b0c0
Cosphi <cosphi@kurajo.ivory.ne.jp>
2022-09-06 00:37:47 +0900

    期待通りの値が返るようになりましたわ。ただ数値34とスペースの34が区別できないのはまだ気になりますの。

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -11,17 +11,27 @@ enum jikutype {
 };
 
 char parse_one(char c, int *out_val, enum jikutype *out_type) {
+    if (c == ' ') *out_type= SPACE;
+    else *out_type= NUMBER;
+    
+    int out_val_tmp = 0;
     do{
-        enum jikutype out_type_prev = *out_type;
-        if (c == 32){
-            *out_type = SPACE;
+        enum jikutype out_type_tmp;
+        if (c == ' '){
+            out_type_tmp = SPACE;
         }else{
-            *out_type = NUMBER;
-            *out_val *= 10;
-            *out_val += c - 48;
+            out_type_tmp = NUMBER;
+            out_val_tmp *= 10;
+            out_val_tmp += c - '0';
         }
-        if (*out_type != out_type_prev) break;
+        if (*out_type != out_type_tmp) break;
     }while((c = cl_getc()) != EOF);
+
+    if (*out_type == NUMBER ) {
+        *out_val = out_val_tmp;
+    }else{
+        *out_val = ' ';
+    }
     return c;
 }
 
@@ -42,15 +52,20 @@ int main() {
 
     char c = cl_getc();
     enum jikutype out_type;
-    if (c == 32){
-        out_type = SPACE;
-    }else{
-        out_type = NUMBER;
-    }
+
 
     c = parse_one(c,&answer1,&out_type);
+    printf("parse_one\n");
+    printf("c is %c, out_val is %d, out_type is %d\n\n",c,answer1,out_type);
+
     c = parse_one(c,&space,&out_type);
+    printf("parse_one\n");
+    printf("c is %c, out_val is %c, out_type is %d\n\n",c,space,out_type);
+
     c = parse_one(c,&answer2,&out_type);
+    printf("parse_one\n");
+    printf("c is %c, out_val is %d, out_type is %d\n\n",c,answer2,out_type);
+
 
     // verity result.
     assert(answer1 == 123);
