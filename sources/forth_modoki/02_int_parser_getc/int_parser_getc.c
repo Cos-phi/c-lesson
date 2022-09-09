53f8167eec5955acdc431541021f68d6e6f891e8
Cosphi <cosphi@kurajo.ivory.ne.jp>
2022-09-09 20:25:39 +0900

    最初にスペースが２つあると初回のパースが数字の0と誤解してしまう問題を修正

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -58,6 +58,25 @@ void test_parse_one_123_456() {
     printf("OK\n");
 }
 
+void test_parse_one_ss123() {
+    // initialize
+    int space = 0;
+    int answer1 = 0;
+    char c = 'S'; 
+    enum tokentype out_type;
+    cl_getc_set_src("  123");
+
+    // run test
+    c = parse_one(c,&space,&out_type);
+    c = parse_one(c,&answer1,&out_type);
+
+    // verity result.
+    assert(c == EOF);
+    assert(space == ' ');
+    assert(answer1 == 123);
+    assert(out_type == NUMBER);
+    printf("OK\n");
+}
 
 int main() {
     int answer1 = 0;
@@ -68,6 +87,7 @@ int main() {
 
     test_parse_one_123();
     test_parse_one_123_456();
+    test_parse_one_ss123();
 
     cl_getc_set_src("123 456");
 
@@ -91,9 +111,9 @@ enum tokentype to_tokentype(char c){
 
 
 char parse_one(char c, int *out_val, enum tokentype *out_type) {
-    *out_type = to_tokentype(c);
-    int cur_val= 0;
     if( c !=' ' && (c<'0'||c>'9') ) c = cl_getc();
+    int cur_val= 0;
+    *out_type = to_tokentype(c);
 
     do{
         enum tokentype cur_type = to_tokentype(c);
