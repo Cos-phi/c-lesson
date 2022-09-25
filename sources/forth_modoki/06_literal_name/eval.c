5d7f9a997f0be0095d668481664294118304a442
Cosphi <cosphi@kurajo.ivory.ne.jp>
2022-09-25 23:45:06 +0900

    eval時に定義された変数が出てきたら中身を書き換えるコードを書きました

--- sources/forth_modoki/06_literal_name/eval.c
+++ sources/forth_modoki/06_literal_name/eval.c
@@ -30,6 +30,46 @@ void def(){
 //    dict_print_all();
 }
 
+static int dict_pos = 0;
+static struct KeyValue dict_array[1024];
+
+void dict_put(char* key, struct Token *token){
+    for(int i = 0; i < dict_pos; i++) {
+        if( streq( key, dict_array[i].key ) ) {
+            dict_array[i].value = *token;
+            return; 
+        }
+    }
+    dict_array[dict_pos].key = key;
+    dict_array[dict_pos].value = *token;
+    dict_pos++;
+}
+int dict_get(char* key, struct Token *out_token){
+    for(int i = 0; i < dict_pos; i++) {
+        if(streq(key, dict_array[i].key)) {
+            *out_token = dict_array[i].value;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void dict_print_all(){
+    for(int i = 0; i < dict_pos; i++) {
+        printf("key: %s, value: %d \n",dict_array[i].key, dict_array[i].value.u.number);
+    }
+}
+
+int isequal_keyvalue(struct KeyValue *keyvalue1, struct KeyValue *keyvalue2){
+    if( keyvalue1->key != keyvalue2->key ){
+        return 0;
+    }else if( 1 != isequal_token( &(keyvalue1->value), &(keyvalue2->value) )){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
 void eval() {
     struct Token token = {UNKNOWN, {0} };
     int ch = EOF;
@@ -49,6 +89,16 @@ void eval() {
                         add_nums();
                     }else if( streq("def", token.u.name) ){
                         def();
+                    }else{// (name ga dict ni attara...){
+                        char* name;
+                        for(int i = 0; i < dict_pos; i++) {
+                            if( streq( token.u.name, dict_array[i].key ) ) {
+                                name = token.u.name;
+                            }
+                        }
+                        struct Token ref_token = {UNKNOWN, {0} };
+                        dict_get(name,&ref_token);
+                        stack_push(&ref_token);
                     }
                     break;
                 case LITERAL_NAME:
@@ -59,6 +109,21 @@ void eval() {
             }
         }
     }while(ch != EOF);
+
+}
+
+static void test_def_and_add() {
+    char *input = "/abc 12 def abc abc add";
+    int expect = 24;
+
+    cl_getc_set_src(input);
+    eval();
+    
+    struct Token actual_token = {UNKNOWN, {0} };
+    stack_pop(&actual_token);
+    int actual = actual_token.u.number;
+
+    assert(expect == actual);
 
 }
 
@@ -74,6 +139,33 @@ static void test_def() {
     
     assert(12 == actual);
 
+}
+
+static void test_isequal_keyvalue(){
+    struct Token input1_value;  input1_value.ltype  = NUMBER; input1_value.u.number  = 123;
+    struct KeyValue input1 = { "key1", input1_value};
+    struct Token input2_value;  input2_value.ltype  = NUMBER; input2_value.u.number  = 123;
+    struct KeyValue input2 = { "key1", input2_value};
+    
+    int ans = isequal_keyvalue(&input1,&input2);
+    
+    assert(1 == ans);
+}
+
+static void test_dict_put() {
+    char* input_key = "key";
+    struct Token input_value;  input_value.ltype  = NUMBER; input_value.u.number  = 123;
+    struct Token expect_value; expect_value.ltype = NUMBER; expect_value.u.number = 123;
+    struct KeyValue expect = {"key", expect_value};
+
+    dict_put(input_key,&input_value);
+    
+    assert( isequal_keyvalue( &(dict_array[0]), &expect ) );
+}
+
+void unit_tests_dict(){
+    test_isequal_keyvalue();
+    test_dict_put();
 }
 
 static void test_eval_num_one() {
@@ -150,6 +242,7 @@ int main() {
     test_eval_num_add2();
     unit_tests_dict();
     test_def();
+    test_def_and_add();
 
     return 0;
 }
