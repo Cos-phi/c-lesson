32f408ba1bc1386d3c2be3c6e4cc0933feb91b72
Cosphi <cosphi@kurajo.ivory.ne.jp>
2022-09-26 22:42:02 +0900

    辞書関連をdict.cに分離

--- sources/forth_modoki/06_literal_name/clesson.h
+++ sources/forth_modoki/06_literal_name/clesson.h
@@ -3,13 +3,6 @@
 #include <string.h>
 #include <assert.h>
 
-/*
-return one character and move cursor.
-return EOF if end of file.
-*/
-int cl_getc();
-void cl_getc_set_src(char* str);
-
 #ifndef STRUCT_H
 #define STRUCT_H
 
@@ -42,16 +35,28 @@ struct KeyValue {
 
 #define NAME_SIZE 256
 #define STACK_SIZE 1024
+
+//cl_getc.c
+int cl_getc();
+void cl_getc_set_src(char* str);
+
+//parce.c
 int parse_one(int prev_ch, struct Token *out_token); 
 
+//stack.c
 void stack_pop(struct Token *out_token);
 void stack_push(struct Token *input_token);
 int isequal_token(struct Token *token1, struct Token *token2);
+void stack_clear();
 
+//eval.c
+void eval();
 int streq(char *s1, char *s2);
 
+//dict.c
 void dict_put(char* key, struct Token *token);
 int dict_get(char* key, struct Token *out_token);
+void def();
 void dict_print_all();
 int isequal_keyvalue(struct KeyValue *keyvalue1, struct KeyValue *keyvalue2);
 void unit_tests_dict();
