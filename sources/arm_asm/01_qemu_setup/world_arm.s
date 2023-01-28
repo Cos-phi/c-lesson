64e1b8db5b2df391d1af9c86793b8e906112df6f
Cosphi <cosphi@kurajo.ivory.ne.jp>
2023-01-28 19:16:46 +0900

    worldしました

--- sources/arm_asm/01_qemu_setup/world_arm.s
+++ sources/arm_asm/01_qemu_setup/world_arm.s
@@ -11,15 +11,15 @@
 .globl _start
 _start:
     ldr r0,=0x101f1000
-    mov r1,#0x68
+    mov r1,#0x77
     str r1,[r0]
-    mov r1,#0x65
+    mov r1,#0x6F
+    str r1,[r0]
+    mov r1,#0x72
     str r1,[r0]
     mov r1,#0x6c
     str r1,[r0]
-    mov r1,#0x6c
-    str r1,[r0]
-    mov r1,#0x6f
+    mov r1,#0x64
     str r1,[r0]
     mov r2,#0x0D
     str r2,[r0]
