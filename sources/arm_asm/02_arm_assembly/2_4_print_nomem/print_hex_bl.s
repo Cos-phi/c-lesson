9ecb74c4df97c1da372fba51212b0854ce197f0b
Cosphi <cosphi@kurajo.ivory.ne.jp>
2023-01-31 00:07:28 +0900

    print_hex is called by bl

--- sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
+++ sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
@@ -12,4 +12,26 @@ end:
 
 
 print_hex:
-  // TODO: write here!
+/* print r0 in the form of hex on UART */
+/* r1 is UART address, r2 is the current charactor */
+/* r3 is counter */
+  ldr r1,=0x101f1000
+  mov r3,#28 
+loop:
+  lsr r2,r0,r3
+  and r2,r2,#0x0F
+  cmp r2,#10
+  add r2,r2,#48
+  blt print
+  add r2,r2,#7
+print:
+  str r2,[r1]
+  sub r3,#4
+  cmp r3,#0
+  bge loop
+  mov r4,#0x0D
+  str r4,[r1]
+  mov r4,#0x0A
+  str r4,[r1]
+  mov r15,r14
+
