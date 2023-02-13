2104bac9351303fd281f95a99effce930e99d74b
Cosphi <cosphi@kurajo.ivory.ne.jp>
2023-02-14 01:08:03 +0900

    endian interpreter

--- sources/arm_asm/04_disasm/cl_utils.c
+++ sources/arm_asm/04_disasm/cl_utils.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <assert.h>
 
-
-
 static char buf[100*1024];
 static char buf2[100*1024];
 
@@ -67,7 +65,23 @@ void cl_printf(char *fmt, ...) {
     }
     va_end(arg_ptr);
 }
+
+int interpret_endian(int word){
+    printf("%x\n",word);
+    int word0 = (word & 0x000000FF) << 24;
+    int word1 = (word & 0x0000FF00) << 8;
+    int word2 = (word & 0x00FF0000) >> 8;
+    int word3 = (word & 0xFF000000) >> 24;
+    int word_interpreted = word0 | word1 | word2 | word3;
+    printf("%x\n",word_interpreted);
+    return word_interpreted;
+}
+
 int print_asm(int word){
+    int word_interpreted    = interpret_endian(word);
+    int immediate_operand   = (word & 0b00000010000000000000000000000000) >> 25;
+    int operation_code      = (word & 0b00000001111000000000000000000000) >> 21;
+    printf ("opecode %x\n",operation_code);
     if(word == 0xE3A01068){
         cl_printf("mov r1, #0x68\n");
         return 1;
