2d1e60206afd88d9d60c1f79a3a09862f1d2832a
Cosphi <cosphi@kurajo.ivory.ne.jp>
2023-05-10 23:57:24 +0900

    とりあえずユニットテストらしきものを作ってみましたわ

--- sources/arm_asm/05_asm/asm.c
+++ sources/arm_asm/05_asm/asm.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+
+int asm_one(char* input){
+    int word;
+    if (0 == strcmp("mov r1, r2" ,input)){
+        word = 0xE1A01002;
+        return word;
+    }else{
+        return 0;
+    }
+}
 
 static void test_asm(){
     char* input = "mov r1, r2";
     int expect = 0xE1A01002; // 1110 00 0 1101 0 0000 0001 00000000 0002
      
-    //int actual = func(input);
+    int actual = asm_one(input);
 
     assert(expect == actual);
 }
