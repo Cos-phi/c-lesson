3ff9975166327e9ce2736ebc3d675107aea26ee1
Cos-phi <cosphi@kurajo.ivory.ne.jp>
2023-11-21 22:01:40 +0900

    1_1 hello.cの実行方法のコメントを修正しました

--- sources/casm_link/01_call_c/1_1_call_c/hello.c
+++ sources/casm_link/01_call_c/1_1_call_c/hello.c
@@ -1,10 +1,9 @@
 /*
- arm-none-eabi-gcc hello.c -nostdlib -o hello.o
+ arm-none-eabi-gcc -c hello.c -nostdlib
  arm-none-eabi-as hello_c.s -o hello_c.o
  arm-none-eabi-ld hello_c.o hello.o -N -Ttext 0x00010000 -o hello_c.elf
  arm-none-eabi-objcopy hello_c.elf -O binary hello_c.bin
  qemu-system-arm -M versatilepb -m 128M -nographic -kernel hello_c.bin -serial mon:stdio
-
 */
 int hello_c() {
     *(volatile char *)0x101f1000 = 'H';
