4e335090f5f7588243f9df96fdd5e8f9c81c7845
Cos-phi <cosphi@kurajo.ivory.ne.jp>
2023-12-07 14:57:47 +0900

    r1のアドレスをint*にキャストしました

--- sources/casm_link/04_c_sources/stack_walk.c
+++ sources/casm_link/04_c_sources/stack_walk.c
@@ -6,7 +6,7 @@ void print_address(int address) {
 
 int func3 (int a4) {
     // TODO: print func2's local avriable a3 here.
-    print_address((int)&a4+4); // r11 のアドレス
+    print_address((int*)((int)&a4+4)); // r11 のアドレス
     return a4*3;
 }
 
