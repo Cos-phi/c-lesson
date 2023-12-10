5ecb54690f91652e42d851f185eb3594a2f07b3a
Cos-phi <cosphi@kurajo.ivory.ne.jp>
2023-12-10 17:03:41 +0900

    asm_cのユニットテストでエラーが出ないよう実行方法を変えました。

--- sources/casm_link/05_inline_asm/mycompfunc_sum_till_jit.sh
+++ sources/casm_link/05_inline_asm/mycompfunc_sum_till_jit.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-./arm_asm.out sum_till_jit_func.ks sum_till_jit_func.bin
+
+cd ../../arm_asm/05_asm
+./a.out ../../casm_link/05_inline_asm/sum_till_jit_func.ks ../../casm_link/05_inline_asm/sum_till_jit_func.bin
