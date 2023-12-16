dfcf8d995aaf34ca0ac38d3b4ac7520eaab84ecd
Cos-phi <cosphi@kurajo.ivory.ne.jp>
2023-12-16 16:49:32 +0900

    sum_till_jit.cからdivfuncにJITに関する部分をコピペしてきました。

--- sources/casm_link/06_jit_ps/div/divfunc.c
+++ sources/casm_link/06_jit_ps/div/divfunc.c
@@ -57,6 +57,36 @@ int div_inline(int a, int b) {
     return res;
 }
 
+/*
+JIT
+*/
+int *binary_buf;
+
+int* allocate_executable_buf(int size) {
+    return (int*)mmap(0, size,
+                 PROT_READ | PROT_WRITE | PROT_EXEC,
+                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+}
+
+void jit_sum_till() {
+    /* TODO:
+    Place binary to binary_buf here.
+    Hint: put almost the same binary as sum_till_inline.
+    Compile and use arm-linux-gnueabi-objdump -S ./a.out to check binary.
+
+    */
+    // dummy implementation 
+    
+    binary_buf[0] = 0xe3a01000; //0x00 mov	r1, #0
+    binary_buf[1] = 0xe3a02000; //0x04 mov	r2, #0
+    binary_buf[2] = 0xe1500001; //0x08 cmp	r0, r1 (label loop:)
+    binary_buf[3] = 0xda000002; //0x0c blt	0x1c (b end)
+    binary_buf[4] = 0xe0822001; //0x10 add	r2, r2, r1
+    binary_buf[5] = 0xe2811001; //0x14 add	r1, r1, #1
+    binary_buf[6] = 0xeafffffa; //0x18 b	0x8 (b loop)
+    binary_buf[7] = 0xe1a00002; //0x1c mov	r0, r2 (label end:)
+    binary_buf[8] = 0xe1a0f00e; //0x20 mov	pc, lr (mov r15, r14)
+}
 
 void main(){
     assert( 6 == div(42, 7));
@@ -68,4 +98,14 @@ void main(){
     assert(-6 == div_inline(42,-7));
     assert(-6 == div_inline(-42,7));
     assert(12 == div_inline(-49,-4));
+
+    int res;
+    int (*funcvar)(int);
+    binary_buf = allocate_executable_buf(1024);
+
+    jit_sum_till();
+
+    funcvar = (int(*)(int))binary_buf;
+    res = funcvar(10);
+    assert(res == 45);
 }
